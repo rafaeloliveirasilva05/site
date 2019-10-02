df87ac6b1d754b502dc1c97165890f1952b9185e
rafael05 <rafaeloliveirasilva05@gmail.com>
2019-10-02 16:32:40 -0300

    organizing the folders and building the screen of the carnival photo gallery 2019

---
+++ js/teste.js
@@ -0,0 +1,38 @@
+
+
+const $containerGalleryPhotos = document.querySelector('.containerGallery')
+const $img = document.querySelectorAll('.card_img')
+const modal = document.querySelector('.containerModal')
+const modal_img = document.querySelector('#modal_img')
+let srcVal = ''
+
+const modal_content = document.querySelector('.modal_content')
+
+sizeOfThings()
+
+window.addEventListener('resize', sizeOfThings)
+
+function sizeOfThings() {
+  let windowWidth = window.innerWidth;
+  let windowClientWidth = $containerGalleryPhotos.clientWidth
+
+  let mg = windowWidth <= 768 ? 8 : 30
+
+  let cal = (windowClientWidth - mg) / 3
+
+  $img.forEach(element => {
+    element.style = `width: ${cal}px; height: ${cal}px`
+  })
+}
+
+$img.forEach((element, index) => {
+  element.addEventListener('click', function(){
+    srcVal = element.getAttribute('src')
+    modal_img.setAttribute('src', srcVal)
+    modal.classList.toggle('modal_active')
+  })
+})
+
+modal_content.addEventListener('click', function() {
+  modal.classList.toggle('modal_active')
+})
